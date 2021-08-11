4244c9ac8a4e886ab1474b1ea0e5554dd71c2665
ruivalente99 <rui.valente99@gmail.com>
2021-08-11 15:46:35 +0100

    HEL-5 #comment Create a To Do App

--- Hello World/hello-world/src/App.js
+++ Hello World/hello-world/src/App.js
@@ -1,8 +1,26 @@
+import {useState} from 'react'
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 function App() {
+  const [tasks,setTasks] = useState(
+    [
+    {
+        id: 1,
+        text: 'hello',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    },
+    {
+        id: 2,
+        text: 'World',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    },
+])
   return (
-    <div className="App">
-      <Header title='Hello'/>
+    <div className="container">
+      <Header title='To Do'/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
