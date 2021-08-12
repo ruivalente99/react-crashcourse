f3664bfb89fb9236b7306255c7e42de0deca76b5
ruivalente99 <rui.valente99@gmail.com>
2021-08-12 13:36:25 +0100

    HEL-5 #comment Some problems fixed - FrontEnd Complete

--- Hello World/hello-world/src/App.js
+++ Hello World/hello-world/src/App.js
@@ -1,7 +1,9 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 function App() {
+  const [showAddTask,setShowAddTask] = useState(false);
   const [tasks,setTasks] = useState(
     [
     {
@@ -17,16 +19,37 @@ function App() {
         reminder: false,
     },
 ])
+//Add Task 
+const addTask = (task) => {
+  const id = Math.floor(Math.random()*1000)+1
+  const newTask = { id,...task}
+  setTasks([...tasks, newTask])
+}
+
 // Delete Tasks
 const deleteTask = (id) => {
  setTasks(tasks.filter((tasks) => tasks.id!==id))
 }
 
+// Toggle reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task)=> task.id === id ? {...task,reminder:!task.reminder}:task) )
+}
+
+
   return (
     <div className="container">
-      <Header title='To Do'/>
+      <Header 
+        title='To Do' 
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      /> 
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length>0 ?
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      <Tasks tasks={tasks} 
+        onDelete={deleteTask}
+        onToggle={toggleReminder}
+      />
       :
       'No tasks to show'
       }
--- Hello World/hello-world/src/components/Task.js
+++ Hello World/hello-world/src/components/Task.js
@@ -1,10 +1,16 @@
 import { FaTimes } from 'react-icons/fa'
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div className="task">
+        <div className={`task ${task.reminder? 'reminder':''}`}
+        onDoubleClick={() => onToggle(task.id)}
+        >
+            
             <h3>
                 {task.text}
-                <FaTimes onClick={() => onDelete(task.id)} style={{color:'red', cursor: 'pointer'}} />
+                <FaTimes 
+                onClick={() => onDelete(task.id)} style={{color:'red', cursor: 'pointer'}} 
+                
+                />
             </h3>
             <p>{task.day}</p>
         </div>
